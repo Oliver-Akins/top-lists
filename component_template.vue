af3c7d9f91075b51a24ce8a6833e5cceb5567d5b
Tyler-A <tyler.akins1999@gmail.com>
2020-07-29 22:49:33 -0600

    Add template Vue file so I don't need to type it anew every time

---
+++ component_template.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id=""></div>
+</template>
+
+<script>
+export default {
+	name: ``,
+	components: {},
+	data() { return {};},
+	computed: {},
+	methods: {}
+}
+</script>
+
+<style>
+
+</style>
