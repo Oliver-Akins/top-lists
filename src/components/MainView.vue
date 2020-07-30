ec2cc38e9eebc8dd0fefa5f5943ebcc5e4f48172
Tyler-A <tyler.akins1999@gmail.com>
2020-07-29 22:50:45 -0600

    Begin work on the main view

---
+++ src/components/MainView.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="main_screen">
+		<Control
+			@export-attempt="handle_export"
+		/>
+	</div>
+</template>
+
+<script>
+import ControlCard from "./ControlBar.vue"
+
+export default {
+	name: ``,
+	components: {
+		Control: ControlCard
+	},
+	data() { return {};},
+	computed: {},
+	methods: {
+		handle_export() {
+			alert("Potato")
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
