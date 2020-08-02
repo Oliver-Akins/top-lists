4b9a978d28f5b395341f2e422d2d2bdd1ef7e4e4
Tyler-A <tyler.akins1999@gmail.com>
2020-08-02 16:13:17 -0600

    Add a mixin to be able to fetch the values from CSS variables.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+// global mixings
+Vue.mixin({
+	methods: {
+		css_var(var_name) {
+			return getComputedStyle(document.documentElement).getPropertyValue(var_name);
+		},
+	}
+});
+
+
 // eslint-disable-next-line
 const app = new Vue({
 	render: h => h(App),
