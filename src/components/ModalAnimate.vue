58f3b97bb372df6bd27b30048e64f5dde3883642
Tyler-A <tyler.akins1999@gmail.com>
2020-08-06 23:15:01 -0600

    Create base file for modal animation

---
+++ src/components/ModalAnimate.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id=""></div>
+</template>
+
+<script>
+export default {
+	name: `AnimateModal`,
+	components: {},
+	data() { return {};},
+	computed: {},
+	methods: {}
+}
+</script>
+
+<style>
+
+</style>
