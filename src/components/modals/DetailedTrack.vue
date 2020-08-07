3a4ceaf430de78f6889ca33283a7d6e626fb4f7a
Tyler-A <tyler.akins1999@gmail.com>
2020-08-07 00:43:45 -0600

    Initialize base file for needed modals.

---
+++ src/components/modals/DetailedTrack.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id=""></div>
+</template>
+
+<script>
+export default {
+	name: ``,
+	components: {},
+	created() {},
+	data() { return {}},
+	computed: {},
+	methods: {}
+}
+</script>
+
+<style>
+
+</style>
