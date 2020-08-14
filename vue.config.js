fa46bb44ef79938dc853f110949f4aa291e766c5
Oliver-Akins <Tyler.Akins1999@gmail.com>
2020-08-13 21:25:15 -0600

    Add config entry for allowing ngrok server hosting

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === `production`
 		? `/top-lists/`
-		: `/`
+		: `/`,
+	devServer: {
+		disableHostCheck: false, // set this to true to allow ngrok web serving
+	},
 }
