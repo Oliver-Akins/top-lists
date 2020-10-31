1d2a38f8cc7c4b5639130c1bd8b2c540cf592983
Oliver-Akins <Tyler.Akins1999@gmail.com>
2020-10-30 21:35:01 -0600

    Make the palette icon actually work properly

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -52,6 +52,9 @@
 			<svg
 				:width="innerSize"
 				:height="innerSize"
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
 			>
 				<rect
 					:width="innerSize"
