e93dedc91b618f6c33293a8977f3e53e9eeff3b3
Oliver-Akins <Tyler.Akins1999@gmail.com>
2021-04-01 00:32:29 -0600

    Add site information to the modal

--- src/components/modals/SiteInfo.vue
+++ src/components/modals/SiteInfo.vue
@@ -1,4 +1,4 @@
-<template id="pop-modal">
+<template id="info-modal">
 	<transition name="fade" @after-enter="content = true">
 		<div
 			v-if="container"
@@ -7,7 +7,31 @@
 		>
 			<transition name="burst" @after-leave="$emit('close')">
 				<div v-if="content" class="modal">
-					<h2 class="center">Website Information</h2>
+					<h2 class="center">Why Does This Exist?</h2>
+					<p>
+						This exists because
+						<a
+							href="https://github.com/Oliver-Akins"
+							target="_blank"
+							rel="noopener"
+						>Oliver</a>
+						wanted some way to be able to see his top songs in durations
+						different than what Spotify distributes on their own (yearly),
+						so he created this website.
+					</p>
+					<hr>
+					<h2>Source Code?</h2>
+					<p>
+						Top Lists is completely open source! You can audit the
+						code at
+						<a
+							href="http://github.com/Oliver-Akins/top-lists"
+							target="_blank"
+							rel="noopener"
+						>
+							github.com/Oliver-Akins/top-lists
+						</a>
+					</p>
 				</div>
 			</transition>
 		</div>
